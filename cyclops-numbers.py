8e6da52ffdaf2b730b97b869e97002d118eba7ed
maplesyrupman <maplesyrupman@protonmail.com>
2020-12-18 23:45:10 -0500

    cyclops-numbers.py yet again, hopefully the last time

--- cyclops-numbers.py
+++ cyclops-numbers.py
@@ -9,18 +9,20 @@ Created on Thu Dec 10 17:38:17 2020
 def is_cyclops(n):
     nstring= str(n)
     mid= len(nstring)//2
-    if len(nstring)== 1 and nstring[0] == '0':
+    if nstring[mid] == '0':
+        if len(nstring) == 1:
+            return True
+        for f in nstring[:mid]:
+            if f == '0':
+                return False
+        reverse= [r for r in nstring[::-1]]
+        for b in reverse[:mid]:
+            if b == '0':
+                return False
         return True
-    for f in nstring[:mid:-1]:
-        if f == '0':
-            return False
-    for b in nstring[mid+1:]:
-        if b == '0':
-            return False
-    return True
-        
+    return False
                  
  
-print(is_cyclops(123404321))
+print(is_cyclops(12344321))
 print(is_cyclops(123404301))
 print(is_cyclops(0))
