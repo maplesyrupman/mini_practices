32c8fe47031b1c9c0406d469e3800f6bdb5a8a3f
maplesyrupman <maplesyrupman@protonmail.com>
2020-12-28 01:45:13 -0500

    added riffle.py

---
+++ riffle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 21:00:35 2020
+
+@author: max
+"""
+
+def riffle(items, out=True):
+    shfl_items= []
+    
+    if items:
+        mid= len(items)//2
+        left= items[:mid]
+        right= items[mid:]
+        
+        while left or right:
+            if out:
+                shfl_items.append(left.pop(0))
+                shfl_items.append(right.pop(0))
+            else:
+                shfl_items.append(right.pop(0))
+                shfl_items.append(left.pop(0))
+                
+    return shfl_items 
+
